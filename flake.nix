94f7481cd973a25e1d33ad32d0ac0721e8f59ce7
Jimmy Brisson <jimmy.brisson@linaro.org>
2022-04-14 13:43:09 -0500

    Pin dev shell with nix flake
    This is not required, it's just more convinient for me

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "The Aerology Devshell";
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/master";
+      flake-utils.url = "github:numtide/flake-utils/master";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            cargo-watch
+            mdbook
+            gdb
+          ];
+        };
+      });
+}
